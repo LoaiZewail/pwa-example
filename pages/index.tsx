9d30a664eac326c1ddfc95e565469ae067cb6152
Loai Zewail <loaizewail@gmail.com>
2023-04-07 02:02:00 +0200

    pwa example

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 style={{ color: "white", fontSize: "110px" }}>Hi ..</h1>
+      </main>
+    </div>
+  )
+}
